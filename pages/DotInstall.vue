56bb12cc0f6a5e7058180f581ea58c4290889013
Akichan-y <a.yamazaki1971@gmail.com>
2021-01-28 14:58:53 +0900

    <HTML>の基礎について完了した

--- pages/DotInstall.vue
+++ pages/DotInstall.vue
@@ -1,7 +1,54 @@
 <template>
   <dir>
-    <h1>ドットインストールを２月中まで再度勉強し直してみる！</h1>
-    <!-- <p>ドットインストールを２月中まで再度勉強し直してみる！</p> -->
+     <head>
+      <title>太郎のポートフォリオサイト</title>
+      <meta name="discription" content="太郎のポートフォリオサイトです。">
+     </head>
+
+    <img src="~/assets/img/taro.png" width="120" height="120" alt="太郎のアイコンです">
+    <header>
+
+      <h1>山田太郎です。</h1>
+      <p>UI/UXデザイナー見習いです。</p>
+      <ul>
+        <li>
+          <a href="https://dotinstall.com" target="_brank">
+          <img src="~/assets/img/blog.png" width="20" height="20" alt="ブログサイトへのリンク画像">
+          </a>
+        </li>
+        <li>
+          <a href="https://dotinstall.com" target="_brank">
+          <img src="~/assets/img/photos.png" width="20" height="20" alt="写真サイトへのリンク画像">
+          </a>
+        </li>
+      </ul>
+    </header>
+    <section>
+
+    <h1>WORKS</h1>
+
+    <section>
+
+    <img src="~/assets/img/work1.png" width="400" height="260" alt="勇者ゲームの紹介画像">
+    <h1>勇者ゲームです</h1>
+    <p>楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！</p>
+    </section>
+    <section>
+
+    <img src="~/assets/img/work2.png" width="400" height="260" alt="宝探しゲームの紹介画像">
+    <h1>宝探しゲームです</h1>
+    <p>楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！</p>
+    </section>
+    <section>
+
+    <img src="~/assets/img/work3.png" width="400" height="260" alt="神経衰弱ゲームの紹介画像">
+    <h1>神経衰弱ゲームです</h1>
+    <p>楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！楽しいゲームです！</p>
+      </section>
+    </section>
+    <footer>
+        <p>copy light Dotinstall</p>
+    </footer>
   </dir>
 </template>
 <style scoped>
